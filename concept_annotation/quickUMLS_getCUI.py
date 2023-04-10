810315e7e845eef4719dfa90382fd571139e495a
Ahmad Albarqawi <barqawi.88@outlook.com>
2023-04-10 07:58:02 +0100

    Update quickUMLS_getCUI.py
    max 10 opened files

--- concept_annotation/quickUMLS_getCUI.py
+++ concept_annotation/quickUMLS_getCUI.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     pool_count = os.cpu_count()-4
     if pool_count > 30:
         pool_count -= 12
-    # max opened files = 12
+    # max opened files = 10
     pool_count = min(pool_count, 10)
     print('number of cpu processing: ', pool_count)
     # pool = Pool(pool_count)
